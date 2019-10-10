a465b6e53533e10613d7fa35ebe707eb3967aeb6
kUUroWraat <56115034+kUUroWraat@users.noreply.github.com>
2019-10-10 14:33:43 +0200

    Add files via upload

---
+++ Labrador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+class Labrador : Dog
+{
+    public Labrador(string Name, int Age, int Whithers, int Length, string Gender, int Weight, string Breed) : base(Name, Age, Whithers, Length, Gender, Weight, Breed)
+    {
+
+    }
+    public override double GetAsTailLength()
+    {
+        double taillength = Length - Withers;
+
+        if (Gender == "Male")
+        {
+            taillength = Length - Withers + 2;
+            return taillength;
+        }
+        else
+        {
+            taillength = Length - Withers;
+            return taillength;
+        }
+
+    }
+    
+}
+
---
+++ Weiner Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Weiner_Dog : Dog
+{
+
+    public Weiner_Dog(string Name, int Age, int Whithers, int Length, string Gender, int Weight, string Breed) : base(Name, Age, Whithers, Length, Gender, Weight, Breed)
+    {
+
+    }
+    public override double GetAsTailLength()
+    {
+        return Length / 4;
+    }
+    
+}
+
